b001b7fd7a951b7856a2514c5df70ff8cf087161
SihamKrd <kerdioui.s@gmail.com>
2023-10-16 09:48:01 +0100

    Isolation du cas Aged Brie

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -16,9 +16,20 @@ class GildedRose {
                 continue;
             }
 
+            //2eme étape : Aged Brie
 
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if(items[i].name.equals("Aged Brie")){
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
+                items[i].sellIn = items[i].sellIn - 1;
+                if (items[i].sellIn < 0 && items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                }
+                continue;
+            }
+
+            if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     items[i].quality = items[i].quality - 1;   
                 }
@@ -42,23 +53,21 @@ class GildedRose {
                 }
             }
 
-            items[i].sellIn = items[i].sellIn - 1;
             
+                items[i].sellIn = items[i].sellIn - 1;
+            
+
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
+
+                                items[i].quality = items[i].quality - 1;
                             
-                            items[i].quality = items[i].quality - 1;
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+                
             }
         }
     }
