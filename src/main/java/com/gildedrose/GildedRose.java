a5dfcce3511311f48dde030cfbab3d188b5872d6
SihamKrd <kerdioui.s@gmail.com>
2023-10-16 09:54:20 +0100

    optimisation des affectations des données dans les variables

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -20,11 +20,11 @@ class GildedRose {
 
             if (items[i].name.equals("Aged Brie")) {
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                    items[i].quality++;
                 }
-                items[i].sellIn = items[i].sellIn - 1;
+                items[i].sellIn--;
                 if (items[i].sellIn < 0 && items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                    items[i].quality++;
                 }
                 continue;
             }
@@ -33,37 +33,35 @@ class GildedRose {
 
             if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                    items[i].quality++;
                     if (items[i].sellIn < 11) {
                         if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
+                            items[i].quality++;
                         }
                     }
 
                     if (items[i].sellIn < 6) {
                         if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
+                            items[i].quality++;
                         }
                     }
                 }
-                items[i].sellIn = items[i].sellIn - 1;
+                items[i].sellIn--;
                 if (items[i].sellIn < 0) {
-                    items[i].quality = items[i].quality - items[i].quality;
+                    items[i].quality = 0;
                 }
                 continue;
             }
 
             if (items[i].quality > 0) {
-                items[i].quality = items[i].quality - 1;
+                items[i].quality--;
             }
 
-            items[i].sellIn = items[i].sellIn - 1;
+            items[i].sellIn--;
 
             if (items[i].sellIn < 0) {
                 if (items[i].quality > 0) {
-
-                    items[i].quality = items[i].quality - 1;
-
+                    items[i].quality--;
                 }
 
             }
