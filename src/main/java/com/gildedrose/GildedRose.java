26bf946290dcbb4505ea630a1443373b054d7888
SihamKrd <kerdioui.s@gmail.com>
2023-10-19 13:38:45 +0100

    Déplacement de la méthode updateQuality() de la classe GlidedRose vers la classe Item

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,47 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-
-            switch(items[i].name){
-                case "Sulfuras, Hand of Ragnaros" : 
-                    break;
-                case "Aged Brie" :
-                    items[i].sellIn--; 
-                    if (items[i].quality < 50) {
-                        items[i].quality++;
-                    }
-                    if (items[i].sellIn < 0 && items[i].quality < 50) {
-                        items[i].quality++;
-                    }
-                    break;
-                case "Backstage passes to a TAFKAL80ETC concert" :
-                    if (items[i].quality < 50) {
-                    items[i].quality++;
-                    if (items[i].sellIn < 11 && items[i].quality < 50) {
-                        items[i].quality++;
-                    }
-                    if (items[i].sellIn < 6 && items[i].quality < 50) {
-                        items[i].quality++;
-                    }
-                    }
-                    items[i].sellIn--;
-                    if (items[i].sellIn < 0) {
-                        items[i].quality = 0;
-                    }
-                    break;
-                default :
-                    items[i].sellIn--;
-                    if (items[i].quality > 0) {
-                        items[i].quality--;
-                    }
-
-                    if (items[i].sellIn < 0) {
-                        if (items[i].quality > 0) {
-                            items[i].quality--;
-                        }
-                    }
-                    break;
-            }
+            items[i].updateQuality();
         }
     }
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -18,4 +18,48 @@ public class Item {
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+    
+    public void updateQuality(){
+        switch(this.name){
+            case "Sulfuras, Hand of Ragnaros" : 
+                break;
+            case "Aged Brie" :
+                this.sellIn--; 
+                if (this.quality < 50) {
+                    this.quality++;
+                }
+                if (this.sellIn < 0 && this.quality < 50) {
+                    this.quality++;
+                }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert" :
+                if (this.quality < 50) {
+                this.quality++;
+                    if (this.sellIn < 11 && this.quality < 50) {
+                        this.quality++;
+                    }
+                    if (this.sellIn < 6 && this.quality < 50) {
+                        this.quality++;
+                    }
+                }
+                this.sellIn--;
+                if (this.sellIn < 0) {
+                    this.quality = 0;
+                }
+                break;
+            default :
+                this.sellIn--;
+                if (this.quality > 0) {
+                    this.quality--;
+                }
+
+                if (this.sellIn < 0) {
+                    if (this.quality > 0) {
+                        this.quality--;
+                    }
+
+                }
+                break;
+        }
+    }
 }
